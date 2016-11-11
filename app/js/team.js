a09e462f902aa19e6304eefa2b06f48a8246e48a
Yuen <edward678679@gmail.com>
2016-11-11 17:12:30 +0800

    Modify the team.html to add the "change leader" & "add leader" parts

--- app/js/team.js
+++ app/js/team.js
@@ -1,3 +1,245 @@
+/*$(document).ready(function(){
+
+	$('#team_page_controller').hide();
+	$('#text_event_name').text("Error: Invalid event name ");
+	var eventName = getURLParameter("q");
+	if (eventName != null && eventName !== '' ) {
+		$('#text_event_name').text("Event name: " + eventName);
+		
+	}
+
+});
+
+angular.module('teamform-team-app', ['firebase'])
+app.controller("MyAuthCtrl", ["$scope", "$firebaseAuth",
+  function($scope, $firebaseAuth) {
+   //your code
+ 	$scope.auth=$firebaseAuth();
+	$scope.auth.$onAuthStateChanged(function(firebaseUser) {
+ 	if (firebaseUser) {
+    	$scope.uid = firebaseUser.uid;
+  	} else {
+    	console.log("Signed out");
+  	}
+	});
+
+		
+	// Call Firebase initialization code defined in site.js
+	initalizeFirebase();
+
+	var refPath = "";
+	var eventName = getURLParameter("q");	
+	
+	// TODO: implementation of MemberCtrl	
+	$scope.param = {
+		"teamName" : '',
+		"currentTeamSize" : 0,
+		"teamMembers" : ["abd", "v"],
+		"teamLeader" : [],
+		"teamLeaderSize" : 0
+		"skills":[];
+		"personality":'';
+		"star": '';
+	};
+		
+	
+
+	refPath =  eventName + "/admin";
+	retrieveOnceFirebase(firebase, refPath, function(data) {	
+
+		if ( data.child("param").val() != null ) {
+			$scope.range = data.child("param").val();
+			$scope.param.currentTeamSize = parseInt(($scope.range.minTeamSize + $scope.range.maxTeamSize)/2);
+			$scope.$apply(); // force to refresh
+			$('#team_page_controller').show(); // show UI
+			
+		} 
+	});
+	
+	
+	refPath = eventName + "/member";	
+	$scope.member = [];
+	$scope.member = $firebaseArray(firebase.database().ref(refPath));
+	
+	
+	refPath = eventName + "/team";	
+	$scope.team = [];
+	$scope.team = $firebaseArray(firebase.database().ref(refPath));
+	
+	
+	$scope.requests = [];
+	$scope.refreshViewRequestsReceived = function() {
+		
+		//$scope.test = "";		
+		$scope.requests = [];
+		var teamID = $.trim( $scope.param.teamName );	
+				
+		$.each($scope.member, function(i,obj) {			
+			//$scope.test += i + " " + val;
+			//$scope.test += obj.$id + " " ;
+			
+			var userID = obj.$id;
+			if ( typeof obj.selection != "undefined"  && obj.selection.indexOf(teamID) > -1 ) {
+				//$scope.test += userID + " " ;
+				
+				$scope.requests.push(userID);
+			}
+		});
+		
+		$scope.$apply();
+		
+	}
+	
+	
+	// set team size
+	$scope.changeCurrentTeamSize = function(delta) {
+		var newVal = $scope.param.currentTeamSize + delta;
+		if (newVal >= $scope.range.minTeamSize && newVal <= $scope.range.maxTeamSize ) {
+			$scope.param.currentTeamSize = newVal;
+		} 
+	}
+
+	$scope.saveFunc = function() {
+		
+		
+		var teamID = $.trim( $scope.param.teamName );
+		
+		if ( teamID !== '' ) {
+			
+			var newData = {				
+				'size': $scope.param.currentTeamSize,
+				'teamMembers': $scope.param.teamMembers
+				'teamLeader': $scope.addleader();
+			};		
+			
+			var refPath = getURLParameter("q") + "/team/" + teamID;	
+			var ref = firebase.database().ref(refPath);
+			
+			
+			// for each team members, clear the selection in /[eventName]/team/
+			
+			$.each($scope.param.teamMembers, function(i,obj){
+				
+				
+				//$scope.test += obj;
+				var rec = $scope.member.$getRecord(obj);
+				rec.selection = [];
+				$scope.member.$save(rec);
+				
+				
+				
+			});
+			
+			
+			
+			ref.set(newData, function(){			
+
+				// console.log("Success..");
+				
+				// Finally, go back to the front-end
+				// window.location.href= "index.html";
+			});
+			
+			
+			
+		}
+		
+		
+	}
+	$scope.loadFunc = function() {
+		
+		var teamID = $.trim( $scope.param.teamName );		
+		var eventName = getURLParameter("q");
+		var refPath = eventName + "/team/" + teamID ;
+		retrieveOnceFirebase(firebase, refPath, function(data) {	
+
+			if ( data.child("size").val() != null ) {
+				
+				$scope.param.currentTeamSize = data.child("size").val();
+				
+				$scope.refreshViewRequestsReceived();
+								
+				
+			} 
+			
+			if ( data.child("teamMembers").val() != null ) {
+				
+				$scope.param.teamMembers = data.child("teamMembers").val();
+				
+			}
+			
+			$scope.$apply(); // force to refresh
+		});
+
+	}
+	
+	$scope.processRequest = function(r) {
+		//$scope.test = "processRequest: " + r;
+		
+		if ( 
+		    $scope.param.teamMembers.indexOf(r) < 0 && 
+			$scope.param.teamMembers.length < $scope.param.currentTeamSize  ) {
+				
+			// Not exists, and the current number of team member is less than the preferred team size
+			$scope.param.teamMembers.push(r);
+			
+			$scope.saveFunc();
+		}
+	}
+	
+	$scope.removeMember = function(member) {
+		
+		var index = $scope.param.teamMembers.indexOf(member);
+		if ( index > -1 ) {
+			$scope.param.teamMembers.splice(index, 1); // remove that item
+			
+			$scope.saveFunc();
+		}
+		
+	}
+	
+	$scope.changeleader = function(member1, member2) {
+
+		var index1 = $scope.param.teamLeader.indexOf(member1);
+		var index2 = $scope.param.teamLeader.indexOf(member2);
+
+		if ( index1 > -1 ) {
+			$scope.param.teamLeader.splice(index1, 1);
+			$scope.param.teamLeader.push(member2.uid);
+
+			$scope.saveFunc();
+		}
+		else {
+			$scope.param.teamLeader.splice(index2, 1);
+			$scope.param.teamLeader.push(member1.uid);
+
+			$scope.saveFunc();
+		}
+	}
+
+	$scope.addleader = function() {
+		if ($scope.param.teamLeaderSize == 0)
+		{
+			$scope.param.teamLeader.push($scope.uid);
+			$scope.param.teamLeaderSize++;
+		}	
+	}
+
+	$scope.addleader = function(member) {
+		
+		if ($scope.param.teamLeaderSize <= currentTeamSize)
+		{
+			$scope.param.teamLeaderSize++;
+			$scope.param.teamLeader.push(member.uid);
+
+			$scope.saveFunc();
+		} 
+	}
+	
+	}]);
+ }
+]);*/
+
 $(document).ready(function(){
 
 	$('#team_page_controller').hide();
@@ -24,7 +266,12 @@ angular.module('teamform-team-app', ['firebase'])
 	$scope.param = {
 		"teamName" : '',
 		"currentTeamSize" : 0,
-		"teamMembers" : []
+		"teamMembers" : ["abd", "v"],
+		"teamLeader" : ["abd"],
+		"teamLeaderSize" : 2,
+		"skills":[],
+		"personality":'',
+		"star": ''
 	};
 		
 	
@@ -187,7 +434,43 @@ angular.module('teamform-team-app', ['firebase'])
 		
 	}
 	
-	
+	$scope.changeleader = function(member1, member2) {
+		//document.getElementById("item1").value;
+		var index1 = $scope.param.teamLeader.indexOf(member1);
+		var index2 = $scope.param.teamLeader.indexOf(member2);
+
+		if ( index1 > -1 ) {
+			$scope.param.teamLeader.splice(index1, 1);
+			$scope.param.teamLeader.push(member2.uid);
+
+			$scope.saveFunc();
+		}
+		else {
+			$scope.param.teamLeader.splice(index2, 1);
+			$scope.param.teamLeader.push(member1.uid);
+
+			$scope.saveFunc();
+		}
+	}
+
+	$scope.addleader = function() {
+		if ($scope.param.teamLeaderSize == 0)
+		{
+			$scope.param.teamLeader.push($scope.uid);
+			$scope.param.teamLeaderSize++;
+		}	
+	}
+
+	$scope.addleader = function(member) {
+		
+		if ($scope.param.teamLeaderSize <= currentTeamSize)
+		{
+			$scope.param.teamLeaderSize++;
+			$scope.param.teamLeader.push(member.uid);
+
+			$scope.saveFunc();
+		} 
+	}
 	
 	
 	
